d97ca9850ad9c5ff3b0866eb0f012c1798fa8145
dileep <bdileep665@gmail.com>
2022-03-05 17:21:20 +0530

    first commit of A.java

--- A.java
+++ A.java
@@ -3,5 +3,14 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("aaaaaaaaaaaa");
+		System.out.println("bbbbbbbbbbbb");
+		System.out.println("bbbbbbbbbbbb");
+		System.out.println("cccccccccccc");
+		System.out.println("cccccccccccc");
+		System.out.println("cccccccccccc");
+		System.out.println("dddddddddddd");
+		System.out.println("dddddddddddd");
+		System.out.println("dddddddddddd");
+		System.out.println("dddddddddddd");
 	}
 }
